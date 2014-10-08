d1d1391e7cf906d4074d4dd0ca082ad8c6e1f078
korgan00 <korgan00@hotmail.com>
2014-10-08 17:44:47 +0200

    .

--- test/unusual/tests/lobbyTest/runner/SequentialTestRunner.scala
+++ test/unusual/tests/lobbyTest/runner/SequentialTestRunner.scala
@@ -2,12 +2,15 @@ package unusual.tests.lobbyTest.runner
 
 import org.scalatest.Sequential
 import unusual.model.LobbyState
+import unusual.tests.SharedTest
 import unusual.tests.lobbyTest.{LobbyVisitorTest, LobbyAuthTest}
 import unusual.tests.simulatorController._
 
 
 class SequentialTestRunner extends Sequential(
-  new InitializerTest
+  {SharedTest.SIZES_ENABLED = SharedTest.DESKTOP
+    new InitializerTest
+  }
   , new LobbyAuthTest(LobbyState.DEFAULT_LOBBY)
   , new LobbyVisitorTest
 
