3dc07fd17590ec9bf9a1919b28c7a06d83d93433
vmendi <vmendi@gmail.com>
2014-10-28 11:08:50 -0700

    Las 3 suites por defecto.

--- test/unusual/tests/runner/SequentialTestRunner.scala
+++ test/unusual/tests/runner/SequentialTestRunner.scala
@@ -1,23 +1,13 @@
 package unusual.tests.runner
 
 import org.scalatest.Sequential
-import unusual.model.{ViewContestState, EnterContestState, LobbyState, Contest}
-import unusual.tests.SharedTest
-
-import unusual.tests.contestDescriptionTest._
 import unusual.tests.contestDescriptionTest.runner.ContestDescriptionSequentialTestRunner
-import unusual.tests.enterContestTest.EnterContestAuthTest
-import unusual.tests.lobbyTest._
 import unusual.tests.enterContestTest.runner.EnterContestSequentialTestRunner
-import unusual.tests.simulatorController._
-import unusual.tests.viewContestEntryTest.ViewContestAuthTest
 import unusual.tests.lobbyTest.runner._
 
 
 class SequentialTestRunner extends Sequential(
   new LobbySequentialTestRunner
-  //, new EnterContestSequentialTestRunner
-  //, new ContestDescriptionSequentialTestRunner
-  //, new ContestDescriptionSequentialTestRunner
-
+  , new EnterContestSequentialTestRunner
+  , new ContestDescriptionSequentialTestRunner
   )
