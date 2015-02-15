e3237144ef94225b27ddb58632b2457eebd8c773
vmendi <vmendi@gmail.com>
2015-02-15 12:33:31 -0800

    Habia sobre-complicado las busquedas de FieldPos

--- test/unusual/model/FieldPos.scala
+++ test/unusual/model/FieldPos.scala
@@ -7,35 +7,23 @@ object FieldPos extends Enumeration {
 
 
   def fromUiText(uiText: String) : FieldPos = {
-
-     // Mapea la FieldPos a los posibles valores en distintos idiomas que pueden aparecer en el UI
-     Map(POS_GOAL_KEEPER -> List("POR", "GK"),
-         POS_DEFENSE -> List("DEF"),
-         POS_MIDDLE -> List("MED", "MID"),
-         POS_FORWARD -> List("DEL", "FWD"),
-         POS_ALL -> List("TODOS", "ALL"))
-
-     // Buscamos la primera entrada del mapa (tuple) en cuya lista este el uiText
-     .find(tuple => tuple._2.contains(uiText))
-
-     // Si la hemos encontrado, la devolvemos. Si no, excepcion.
-     match {
-       case Some(v) => v._1
-       case _ => throw new Exception(s"FieldPos position not found for uiText $uiText")
+    uiText match {
+      case "POR" | "GK"    => POS_GOAL_KEEPER
+      case "DEF"           => POS_DEFENSE
+      case "MED" | "MID"   => POS_MIDDLE
+      case "DEL" | "FWD"   => POS_FORWARD
+      case "TODOS" | "ALL" => POS_ALL
+      case _ => throw new Exception(s"FieldPos position not found for uiText $uiText")
     }
   }
 
   def fromCss(cssClasses: String) : FieldPos = {
-
-    // Mapeo de una cssClass a una posicion. Buscamos entre todas las que nos pueden pasar
-    Map("posPOR" -> POS_GOAL_KEEPER,
-        "posDEF" -> POS_DEFENSE,
-        "posMED" -> POS_MIDDLE,
-        "posDEL" -> POS_FORWARD)
-      .find(tuple => cssClasses.contains(tuple._1))
-      match {
-        case Some(pos) => pos._2
-        case _ =>  throw new Exception(s"FieldPos position not found for cssClasses $cssClasses")
+    cssClasses match {
+      case c if c.contains("posPOR") => POS_GOAL_KEEPER
+      case c if c.contains("posDEF") => POS_DEFENSE
+      case c if c.contains("posMED") => POS_MIDDLE
+      case c if c.contains("posPOR") => POS_FORWARD
+      case _ => throw new Exception(s"FieldPos position not found for cssClasses $cssClasses")
     }
   }
 }
